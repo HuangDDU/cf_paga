3ef218af403994a55f7c00651109eb942337b2a5
黄照样 <554598474@qq.com>
2025-03-12 15:09:00 +0800

    workflow docker push

--- test_docker.sh
+++ test_docker.sh
@@ -2,5 +2,5 @@ docker run \
     -it --rm \
     -v ./tmp:/data \
     --workdir /code \
-    cfe/cf_paga:0.0.1\
+    cf_paga:0.0.1\
     python /code/run.py --adata_path /data/adata.h5ad --prior_information /data/prior_information.json --parameters /data/parameters.json --output_filename /data/output.pkl
